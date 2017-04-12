5d032af9d2ae679783a29eb750b5ee68df97cb22
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-04-12 01:07:02 -0600

    Add theme support
    Theme support for thumbnails, HTML5 search form.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
   /**
-  * Load scrips and style files
+  * SCRIPTS & STYLES
   */
    add_action("wp_enqueue_scripts", "vagap_load_assets");
    function vagap_load_assets(){
@@ -14,7 +14,39 @@
 
 
    /**
-   * Add sidebars
+   * THEME SUPPORT
+   */
+   add_theme_support( 'post-thumbnails' );
+   add_theme_support('html5', array(
+     'search-form'
+   ));
+
+
+
+
+   /**
+   * CUSTOMIZER
+   */
+   add_action('after_setup_theme', 'vagap_setup');
+   function vagap_setup() {
+     add_image_size('vagap-logo', 160, 90);
+     add_theme_support('custom-logo', array(
+         'size' => 'vagap-logo'
+     ));
+   }
+
+
+   /**
+   * NAVIGATION MENU
+   */
+   add_action( 'init', 'vagap_menu');
+   function vagap_menu() {
+     register_nav_menu('header-nav',__( 'Header Navigation' ));
+   }
+
+
+   /**
+   * SIDEBARS
    */
    add_action("widgets_init", "vagap_sidebars");
    function vagap_sidebars(){
