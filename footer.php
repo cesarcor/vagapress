63f383dddbf7d8896a0529a56e7e9216f0e46ba4
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-04-13 00:13:16 -0600

    Add widget areas to footer
    initial testing of widget areas in footer

--- footer.php
+++ footer.php
@@ -1,9 +1,31 @@
 
   <footer class="container-fluid footer">
     <div class="row">
-      <div class="col-md-4"></div>
-      <div class="col-md-4"></div>
-      <div class="col-md-4"></div>
+
+
+      <div class="col-md-4">
+        <?php
+         dynamic_sidebar('ftr_col_1');
+        ?>
+
+      </div>
+      <div class="col-md-4">
+        <?php
+         dynamic_sidebar('ftr_col_2');
+        ?>
+
+      </div>
+      <div class="col-md-4">
+        <?php
+         dynamic_sidebar('ftr_col_3');
+        ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <span>Developed by <a href="http://www.cesardowns.com">Cesar Correchel Downs</a></span>
+
+      </div>
     </div>
   </footer>
   <?php wp_footer(); ?>
