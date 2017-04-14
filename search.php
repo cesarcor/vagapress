83f31d593daa78a20bf11ba2138eb17a24bd1c5f
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-04-13 23:09:01 -0600

    search page template dev start

---
+++ search.php
@@ -0,0 +1,24 @@
+<?php
+  get_header();
+?>
+
+ <main class="container-fluid">
+   <div class="row">
+     <div class="col-md-9">
+       <?php get_template_part('template-parts/content/content', 'search'); ?>
+     </div>
+
+     <div class="col-md-3">
+
+     </div>
+
+   </div>
+
+ </main>
+
+
+
+
+<?php
+  get_footer();
+?>
