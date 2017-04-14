8bdc5bb8d1f22c40798222726ff523a01c46bb95
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-04-13 23:08:33 -0600

    n/a

--- header.php
+++ header.php
@@ -2,29 +2,34 @@
 
 <html lang="en">
 <head>
-	<title><?php wp_title(''); ?></title>
+	<title><?php bloginfo('name') ?></title>
 	<?php wp_head(); ?>
 </head>
 
 <body>
 	<header class="header container-fluid">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-4 col-xs-6">
 				<?php
-				      the_custom_logo();
-
-				      if(!the_custom_logo()):
-				      ?>
-
-				<h1 class="header-blogname"><?php bloginfo('name'); ?>
-				</h1>
+				if(has_custom_logo()):
+				?>
+				<div class="header-logo">
+					<?php
+					 the_custom_logo();
+					?>
+				</div>
+			 <?php
+			 else:
+				 ?>
+				<h1 class="header-blogname"><?php bloginfo('name'); ?></h1>
 				<?php
-				      endif;
+				endif;
 				      ?>
 			</div>
 
 
-			<div class="col-md-4">
+			<div class="col-md-4 col-xs-6">
+
 				<nav class="header-menu">
 					<?php
 					        wp_nav_menu( array(
@@ -36,7 +41,13 @@
 			</div>
 
 
-			<div class="col-md-4">
+			<div class="col-md-4 col-xs-12">
+				<div class="vagap-social-header">
+					<a href="#" class="vagap-social-header__links vagap-social-header__links_fb"><i class="fa fa-facebook" aria-hidden="true"></i></a><!--
+					--><a href="#" class="vagap-social-header__links vagap-social-header__links_tw"><i class="fa fa-twitter" aria-hidden="true"></i></a><!--
+					--><a href="#" class="vagap-social-header__links vagap-social-header__links_in"><i class="fa fa-instagram" aria-hidden="true"></i></a><!--
+					--><a href="#" class="vagap-social-header__links vagap-social-header__links_pi"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+				</div>
 				<?php get_search_form(); ?>
 			</div>
 
