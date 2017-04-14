c9ec2834044897f6f386a88666610e03550ae96b
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-04-13 23:09:44 -0600

    add targeted class name to search from

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+<form role="search" method="get" class="search-form header-search" action="<?php echo home_url('/'); ?>">
 		<label>
 			<input type="search"
        class="search-field"
