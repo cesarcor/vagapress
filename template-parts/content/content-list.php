d75be91da640324bb8dd07d50affc50887ee95ae
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-04-13 23:10:05 -0600

    modify list content template

--- template-parts/content/content-list.php
+++ template-parts/content/content-list.php
@@ -1,14 +1,14 @@
 <?php $home_posts_list_query = new WP_Query(array( 'post_count'=> 5, )); ?>
 
 
-<div class="col-md-8">
+<div class="col-md-9">
   <?php if ( $home_posts_list_query->have_posts() ) : ?>
    <?php while ( $home_posts_list_query->have_posts() ) : $home_posts_list_query->the_post(); ?>
 
     <?php if((int) $home_posts_list_query->current_post >= 3): ?>
 
     <div class="list-post">
-      <img src="<?php the_post_thumbnail_url( 'medium' );?>" class="portrait" alt=""><!--
+      <img src="<?php the_post_thumbnail_url( 'large' );?>" class="list-post-thumbnail" alt=""><!--
       --><div class="list-post-info">
         <h2 class="list-post-info__title"><?php the_title(); ?></h2>
         <p>published by <span class="list-post-info__author"><?php the_author_posts_link(); ?></span>
