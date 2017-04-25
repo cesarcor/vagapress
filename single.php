8b4463056d5c070728de2bf712916024aad724f4
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-04-25 01:00:17 -0600

    developing single post template

--- single.php
+++ single.php
@@ -2,10 +2,25 @@
  get_header();
 ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
-<h3><?php the_category('&nbsp;&rsaquo;&nbsp;'); echo "&nbsp;&rsaquo;&nbsp;"; the_title(); ?></h3>
-<?php the_content(); ?>
-<?php endwhile; ?>
+  <main class="container-fluid single-post">
+
+    <section class="row">
+      <article class="col-md-8 single-post__article">
+        <?php while ( have_posts() ) : the_post(); ?>
+        <h3 class="single-post__title"><?php the_title(); ?></h3>
+        <?php the_content(); ?>
+        <?php endwhile; ?>
+      </article>
+
+      <aside class="col-md-4">
+        <?php get_sidebar(); ?>
+      </aside>
+
+    </section>
+
+
+
+  </main>
 
 <?php
  get_footer();
